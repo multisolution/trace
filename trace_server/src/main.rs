4504e1fc87a2d7558fe11f0ebbaccc6ef400c119
leocavalcante <lc@leocavalcante.com>
2020-02-06 08:07:13 -0300

    Hello world

--- trace_server/src/main.rs
+++ trace_server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::any()
+        .map(|| "Hello world");
+
+    let mut args = std::env::args();
+    let port = args.nth(1).unwrap_or("3030".to_string());
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], port.parse().unwrap()))
+        .await
 }
